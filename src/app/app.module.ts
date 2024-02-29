1b0f9d910a487a82c5ec2b126493408b92dae5c0
Queiroz1992 <51212831+Queiroz1992@users.noreply.github.com>
2024-02-29 12:02:52 -0300

    criação do projeto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadClienteComponent } from './cad-cliente/cad-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadClienteComponent
   ],
   imports: [
     BrowserModule,
